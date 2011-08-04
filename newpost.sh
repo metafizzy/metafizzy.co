63f782c8ffa2eae66968065662d1e40cfaed5387
David DeSandro <david@desandro.com>
2011-08-04 10:06:02 -0400

    newpost.sh : remove COPY_FILE_EXT

--- newpost.sh
+++ newpost.sh
@@ -2,15 +2,12 @@
 
 # creates a new post
 # format as _posts/YYYY/YYYY-MM-DD-filename.ext
-# usage
-# $ ./newpost.sh my-new-post-filename
+# usage:
+#   $ ./newpost.sh my-new-post-filename
 
 COPY_FILE=_posts/template.mdown
-# get file extension http://unix.stackexchange.com/questions/1571/bash-get-file-extension
-COPY_FILE_NAME=$(basename $COPY_FILE)
-COPY_FILE_EXT=${COPY_FILE_NAME##*.}
 # create file
-POST_FILE=_posts/$(date "+%Y/%Y-%m-%d")-$1.$COPY_FILE_EXT
+POST_FILE=_posts/$(date "+%Y/%Y-%m-%d")-$1.mdown
 
 echo new post: $POST_FILE
 cp $COPY_FILE $POST_FILE
