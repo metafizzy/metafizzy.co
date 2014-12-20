d7ed5b9c3176e95a0bf86cca3d02e9fd0749b67c
David DeSandro <desandrocodes@gmail.com>
2014-12-20 15:21:49 -0500

    refactor css into separate files
    add grunt for concat

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+// -------------------------- grunt -------------------------- //
+
+/*jhint node: true, usued: true, undef: true */
+
+var cssFiles = [
+  'assets/css/normalize.css',
+  'assets/css/base.css',
+  'assets/css/transition-links.css',
+  'assets/css/homepage.css',
+  'assets/css/blog.css',
+  'assets/css/order-status.css',
+  'assets/css/syntax-highlight.css',
+  'assets/css/404.css'
+];
+
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+    concat: {
+      css: {
+        src: cssFiles,
+        dest: 'assets/css/style.css'
+      }
+    },
+
+    watch: {
+      content: {
+        files: cssFiles,
+        tasks: [ 'concat:css' ]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask( 'default', [
+    'concat'
+  ]);
+
+};
