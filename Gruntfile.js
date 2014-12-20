a2f7e98c6f205939e094a2c6a92b8b3c52b4c3fb
David DeSandro <desandrocodes@gmail.com>
2014-12-20 15:46:30 -0500

    css tweaks post refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,7 @@ module.exports = function( grunt ) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask( 'default', [
     'concat'
