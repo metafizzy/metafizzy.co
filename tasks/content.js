ac490829db844b3af9be3b198903d614f5c7aed4
David DeSandro <desandrocodes@gmail.com>
2015-10-26 09:44:51 -0400

    create blog post permalinks and pages

--- tasks/content.js
+++ tasks/content.js
@@ -28,62 +28,104 @@ gulp.task( 'content-homepage', function() {
 
 // ----- blog ----- //
 
+var moment = require('moment');
+var marked = require('gulp-marked');
+
 // array of all posts
 var posts = [];
 var postsPerPage = 6;
 // hash of posts per page
-var pagePosts = {};
+var paginatedPosts = [];
 
 gulp.task( 'posts', function() {
+  var rePostPath = /(\d\d\d\d\-\d\d\-\d\d)\-([\w\d\-_]+)/;
+
   return gulp.src('_posts/2015/**.md')
-    // .pipe( frontMatter({
-    //   property: 'frontMatter',
-    //   remove: true
-    // }) )
+    .pipe( frontMatter({
+      property: 'frontMatter',
+      remove: true
+    }) )
     .pipe( through.obj( function( file, encoding, callback ) {
       var basename = path.basename( file.path, path.extname( file.path ) );
       // get dateCode and slug
-      var matches = basename.match(/(\d\d\d\d\-\d\d\-\d\d)\-([\w\d\-_]+)/);
+      var matches = basename.match( rePostPath );
       file.dateCode = matches[1];
       file.slug = matches[2];
+      file.date = new Date( file.dateCode ); // used for sorting
+      // file data
+      file.timestamp = moment( file.date ).format('D MMM YYYY');
+      file.title = file.frontMatter.title;
+
       // get markdown html
-      var contents = file.contents.toString();
-      file.contentHTML = 
-      
+      // var contents = file.contents.toString();
+      // file.contentHTML =
+
       posts.push( file );
       return callback( null, file );
     },
-    function( callback ) {
-      // sort newest first
-      posts.sort( function( a, b ) {
-        return a.dateCode < b.dateCode;
-      });
+      // flush function: sort posts and put into pages
+      function( callback ) {
+        // sort by date
+        posts.sort( function( a, b ) {
+          return b.date - a.date;
+        });
+        // arrange in pages
+        posts.forEach( function( postFile, i ) {
+          var pageIndex = Math.floor( i / postsPerPage );
+          // add new array of posts if not there
+          paginatedPosts[ pageIndex ] = paginatedPosts[ pageIndex ] || [];
+          paginatedPosts[ pageIndex ].push( postFile );
+        });
 
-      posts.forEach( function( postFile, i ) {
-        var pageIndex = Math.ceil( i / postsPerPage );
-        pagePosts[ pageIndex ] = pagePosts[ pageIndex ] || [];
-        pagePosts[ pageIndex ].push( postFile );
-      });
-
-      callback();
-    }) );
-
-
-});
-
-gulp.task('content-blog', [ 'posts' ], function() {
-
-  var data = {
-    posts: posts
-  };
-  return gulp.src('pages/blog.mustache')
-    .pipe( template( data ) )
-    .pipe( rename('blog/index.html') )
+        callback();
+      }
+    ))
+    .pipe( marked() )
+    // create post permalink pages
+    .pipe( rename( function( postPath ) {
+      var matches = postPath.basename.match( rePostPath );
+      postPath.dirname = 'blog/' + matches[2];
+      postPath.basename = 'index';
+      postPath.extname = '.html';
+    }))
     .pipe( gulp.dest('build') );
+
 });
+
+gulp.task( 'content-blog', [ 'posts' ], function() {
+
+  paginatedPosts.forEach( function( pagePosts, i ) {
+    var currentPage = i + 1;
+    var data = {
+      pagePosts: pagePosts,
+      currentPage: currentPage,
+      nextPage: paginatedPosts[ i+1 ] ? currentPage + 1 : null,
+      totalPages: paginatedPosts.length
+    };
+    // previous page
+    if ( currentPage > 1 ) {
+      data.previousPage = {
+        url: currentPage == 2 ? '/blog' : '/blog/page' + ( currentPage - 1 ),
+        index: currentPage - 1
+      };
+    }
+
+    var filename = currentPage == 1 ? 'blog/index.html' :
+      'blog/page' + currentPage + '/index.html';
+
+    gulp.src('pages/blog.mustache')
+      .pipe( template( data ) )
+      .pipe( rename( filename ) )
+      .pipe( gulp.dest('build') );
+  });
+
+});
+
+
 
 // ----- content ----- //
 
 gulp.task( 'content', [
-  'content-homepage'
+  'content-homepage',
+  'content-blog'
 ]);
