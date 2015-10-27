342986439f7c8cd0afc854a587e6543740f914b2
David DeSandro <desandrocodes@gmail.com>
2015-10-26 20:32:24 -0400

    use post partial; build permalinks

--- tasks/content.js
+++ tasks/content.js
@@ -2,35 +2,10 @@ var gulp = require('gulp');
 var through = require('through2');
 var Handlebars = require('handlebars');
 var rename = require('gulp-rename');
-var frontMatter = require('gulp-front-matter');
 var path = require('path');
+var fs = require('fs');
 
-function template( data ) {
-  // defaults
-  data = data || {};
-
-  return through.obj( function( file, encoding, callback ) {
-    var fileContents = file.contents.toString();
-    var tmpl = Handlebars.compile( fileContents );
-    file.contents = new Buffer( tmpl( data ) );
-    return callback( null, file );
-  });
-}
-
-// ----- homepage ----- //
-
-gulp.task( 'content-homepage', function() {
-  return gulp.src('pages/homepage.mustache')
-    .pipe( template() )
-    .pipe( rename('index.html') )
-    .pipe( gulp.dest('build') );
-});
-
-// ----- blog ----- //
-
-var moment = require('moment');
-var marked = require('gulp-marked');
-var highlight = require('highlight.js');
+// ----- vars ----- //
 
 // array of all posts
 var posts = [];
@@ -38,25 +13,44 @@ var postsPerPage = 6;
 // hash of posts per page
 var paginatedPosts = [];
 
-gulp.task( 'posts', function() {
-  var rePostPath = /(\d\d\d\d\-\d\d\-\d\d)\-([\w\d\-_]+)/;
+// -------------------------- posts -------------------------- //
+
+var frontMatter = require('gulp-front-matter');
+var moment = require('moment');
+var marked = require('gulp-marked');
+var highlight = require('highlight.js');
+
+var rePostPath = /(\d\d\d\d\-\d\d\-\d\d)\-([\w\d\-_]+)/;
+
+gulp.task( 'posts', [ 'partials' ], function() {
+
+  var blogPermalinkSrc = fs.readFileSync( 'pages/blog-permalink.mustache', 'utf8' );
+  var blogPermalinkTemplate = Handlebars.compile( blogPermalinkSrc );
 
   return gulp.src('_posts/2015/**.md')
     .pipe( frontMatter({
       property: 'frontMatter',
       remove: true
     }) )
+    .pipe( marked({
+      highlight: function( code ) {
+        return highlight.highlightAuto( code ).value;
+      }
+    }) )
     .pipe( through.obj( function( file, encoding, callback ) {
       // get dateCode and slug from basename
-      var basename = path.basename( file.path, path.extname( file.path ) );
+      var basename = getBasename( file.path );
       var matches = basename.match( rePostPath );
+      if ( !matches || !matches.length ) {
+        console.log( matches, file.path );
+      }
       file.dateCode = matches[1];
       file.slug = matches[2];
       // file data
       file.date = new Date( file.dateCode ); // used for sorting
       file.timestamp = moment( file.date ).format('D MMM YYYY');
       file.title = file.frontMatter.title;
-
+      // add file to posts collection
       posts.push( file );
       return callback( null, file );
     },
@@ -77,11 +71,14 @@ gulp.task( 'posts', function() {
         callback();
       }
     ))
-    .pipe( marked({
-      highlight: function( code ) {
-        return highlight.highlightAuto( code ).value;
-      }
-    }) )
+    // templating
+    .pipe( through.obj( function( file, encoding, callback ) {
+      var data = {
+        post: file
+      };
+      file.contents = new Buffer( blogPermalinkTemplate( data ) );
+      callback( null, file );
+    }))
     // create post permalink pages
     .pipe( rename( function( postPath ) {
       var matches = postPath.basename.match( rePostPath );
@@ -93,7 +90,47 @@ gulp.task( 'posts', function() {
 
 });
 
-gulp.task( 'content-blog', [ 'posts' ], function() {
+// -------------------------- partials -------------------------- //
+
+// register all modules/*/*.mustache files as partials
+gulp.task( 'partials', function() {
+  return gulp.src('modules/*/*.mustache')
+    .pipe( through.obj( function( file, encoding, callback ) {
+      var name = getBasename( file.path );
+      var tmpl = file.contents.toString();
+      Handlebars.registerPartial( name, tmpl );
+      callback( null, file );
+    }));
+});
+
+// -------------------------- content -------------------------- //
+
+// templating plugin, builds content with Handlebars
+function template( data ) {
+  data = data || {};
+
+  return through.obj( function( file, encoding, callback ) {
+    var fileContents = file.contents.toString();
+    var tmpl = Handlebars.compile( fileContents );
+    file.contents = new Buffer( tmpl( data ) );
+    return callback( null, file );
+  });
+}
+
+// ----- homepage ----- //
+
+gulp.task( 'content-homepage', [ 'posts', 'partials' ], function() {
+  return gulp.src('pages/homepage.mustache')
+    .pipe( template({
+      homepagePosts: posts.slice( 0, 5 )
+    }) )
+    .pipe( rename('index.html') )
+    .pipe( gulp.dest('build') );
+});
+
+// ----- blog ----- //
+
+gulp.task( 'content-blog', [ 'posts', 'partials' ], function() {
 
   paginatedPosts.forEach( function( pagePosts, i ) {
     var currentPage = i + 1;
@@ -122,11 +159,15 @@ gulp.task( 'content-blog', [ 'posts' ], function() {
 
 });
 
-
-
 // ----- content ----- //
 
 gulp.task( 'content', [
   'content-homepage',
   'content-blog'
 ]);
+
+// -------------------------- utils -------------------------- //
+
+function getBasename( filepath ) {
+  return path.basename( filepath, path.extname( filepath ) );
+}
