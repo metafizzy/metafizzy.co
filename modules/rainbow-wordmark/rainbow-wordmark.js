544083e134de11585fbae4cea6bfc5d5d3dc1824
David DeSandro <desandrocodes@gmail.com>
2015-11-28 21:51:23 -0500

    render rainbow wordmark with single image

--- modules/rainbow-wordmark/rainbow-wordmark.js
+++ modules/rainbow-wordmark/rainbow-wordmark.js
@@ -13,33 +13,38 @@ if ( !wordmark ) {
 
 var wordmarkImg = wordmark.querySelector('.rainbow-wordmark__image');
 var canvas = wordmark.querySelector('.rainbow-wordmark__canvas');
-
 var ctx = canvas.getContext('2d');
 var trailCount = 100;
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
 
-// ----- load images ----- //
+// ----- load image ----- //
 
-var imgs = {};
+var whiteImg = new Image();
+whiteImg.onload = onWhiteImgLoad;
+whiteImg.src = '/img/wordmark-white.png';
 
-loadImg('/img/wordmark-blue.png');
-loadImg('/img/wordmark-gold.png');
-loadImg('/img/wordmark-orange.png');
-loadImg('/img/wordmark-magenta.png');
-
-function loadImg( url ) {
-  var img = new Image();
-  imgs[ url ] = img;
-  img.onload = onImgLoad;
-  img.src = url;
+function onWhiteImgLoad() {
+  setColorCanvas( 'blue', '#19F' );
+  setColorCanvas( 'gold', '#EA0' );
+  setColorCanvas( 'orange', '#E62' );
+  setColorCanvas( 'magenta', '#C25' );
+  animate();
 }
 
-var loadCount = 0;
+var colorCanvases = {};
 
-function onImgLoad() {
-  loadCount++;
-  if ( loadCount === 4 ) {
-    animate();
-  }
+// get a canvas with the logotype rendered in a color
+function setColorCanvas( name, color ) {
+  var colorCanvas = document.createElement('canvas');
+  colorCanvas.width = whiteImg.width;
+  colorCanvas.height = whiteImg.height;
+  var colorCtx = colorCanvas.getContext('2d');
+  colorCtx.drawImage( whiteImg, 0, 0 );
+  colorCtx.globalCompositeOperation = 'source-in';
+  colorCtx.fillStyle = color;
+  colorCtx.fillRect( 0, 0, whiteImg.width, whiteImg.height );
+  colorCanvases[ name ] = colorCanvas;
 }
 
 // ----- animate rainbow ----- //
@@ -62,12 +67,7 @@ wordmarkImg.onmouseleave = function() {
   isHovering = false;
 };
 
-var imgCycle = [
-  imgs['/img/wordmark-blue.png'],
-  imgs['/img/wordmark-gold.png'],
-  imgs['/img/wordmark-orange.png'],
-  imgs['/img/wordmark-magenta.png']
-];
+var colorCycle = [ 'blue', 'gold', 'orange', 'magenta' ];
 
 function animate() {
   update();
@@ -78,26 +78,25 @@ function animate() {
 function update() {
   t++;
 
-  var colorCycleIndex = Math.floor( t / 10 ) % 4;
-  var cycleImg = imgCycle[ colorCycleIndex ];
-  var nextImg = isHovering ? cycleImg : null;
+  var colorCycleIndex = Math.floor( t / 8 ) % 4;
+  var nextColor = isHovering ? colorCycle[ colorCycleIndex ] : null;
 
   rainbow.pop();
   rainbow.pop();
   rainbow.pop();
-  rainbow.unshift( nextImg );
-  rainbow.unshift( nextImg );
-  rainbow.unshift( nextImg );
+  rainbow.unshift( nextColor );
+  rainbow.unshift( nextColor );
+  rainbow.unshift( nextColor );
 }
 
 function render() {
-  ctx.clearRect(0, 0, 1100, 400);
+  ctx.clearRect( 0, 0, canvasWidth, canvasHeight );
 
   // iterate backwards through rainbow
   for ( var i = rainbow.length-1; i >= 0; i-- ) {
-    var img = rainbow[i];
-    if ( img ) {
-      ctx.drawImage( img, i+1, i+1 );
+    var color = rainbow[i];
+    if ( color ) {
+      ctx.drawImage( colorCanvases[ color ], i+1, i+1 );
     }
   }
 }
